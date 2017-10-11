d1ccd03694c29b5956297488ffb3baee418de1ec
kangxin <k506855660@163.com>
2017-10-11 01:17:19 -0700

    add dectorator

--- structural/decorator.py
+++ structural/decorator.py
@@ -1,10 +1,29 @@
 """
 The decorator is not python decorator.
 """
+import abc 
 
 import functools
 
-class TextTag(object):
+# abstract component
+class Component(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def render(self):
+        """ """
+
+# abstract decorator
+class Decorator(Component, metaclass=abc.ABCMeta):
+
+    def __init__(self, wrapped):
+        self._wrapped = wrapped
+
+    @abc.abstractmethod
+    def render(self):
+        """ """
+
+#ConcreteComponent
+class TextTag(Component):
     """Represents a base text tag"""
     def __init__(self, text):
         self._text = text
@@ -13,22 +32,21 @@ class TextTag(object):
         return self._text
 
 
-class BoldWrapper(TextTag):
+# ConcreteDecorator
+class BoldWrapper(Decorator):
     """Wraps a tag in <b>"""
-    def __init__(self, wrapped):
-        self._wrapped = wrapped
 
     def render(self):
         return "<b>{}</b>".format(self._wrapped.render())
 
 
-class ItalicWrapper(TextTag):
+# ConcreteDecorator
+class ItalicWrapper(Decorator):
     """Wraps a tag in <i>"""
-    def __init__(self, wrapped):
-        self._wrapped = wrapped
 
     def render(self):
         return "<i>{}</i>".format(self._wrapped.render())
+
 
 # def bold_wrapper(func):
 
@@ -55,5 +73,4 @@ if __name__ == '__main__':
     print("before:", simple_hello.render())
     print("after:", special_hello.render())
 
-    print('-----------------------------')
 
